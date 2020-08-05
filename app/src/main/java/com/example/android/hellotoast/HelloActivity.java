aaa187f52cfde2bcb6f02c1a9fa8e57539023d28
kerta_123456 <kuanwy-wm19@student.tarc.edu.my>
2020-08-05 16:24:14 +0800

    By KUan

---
+++ app/src/main/java/com/example/android/hellotoast/HelloActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.hellotoast;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class HelloActivity extends AppCompatActivity {
+
+
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        textView.setText(message);
+    }
+
+    /*
+     * Shows a Toast when the TOAST button is clicked.
+     *
+     * @param view The view that triggered this onClick handler.
+     *             Since a toast always shows on the top,
+     *             the passed in view is not used.
+     */
+
+
+    /*
+     * Increments the number in the TextView when the COUNT button is clicked.
+     *
+     * @param view The view that triggered this onClick handler.
+     *             Since the count always appears in the TextView,
+     *             the passed in view is not used.
+     */
+
+}
